73267ec498b284a5dbce8314aeef6a2eadc5233b
Fran Dios <frankdiox@gmail.com>
2018-04-28 22:34:40 +0900

    Feat: Improve errors

--- server/src/errors/index.js
+++ server/src/errors/index.js
@@ -1,9 +1,22 @@
-class AuthError extends Error {
-  constructor() {
-    super('Not authorized')
+class DefaultError extends Error {
+  constructor(...messages) {
+    super(messages.join('. ') + '.')
+  }
+}
+
+class AuthError extends DefaultError {
+  constructor(...messages) {
+    super('Not authorized', ...messages)
+  }
+}
+
+class BadRequestError extends DefaultError {
+  constructor(...messages) {
+    super('Bad request', ...messages)
   }
 }
 
 module.exports = {
   AuthError,
+  BadRequestError,
 }
