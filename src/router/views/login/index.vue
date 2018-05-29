7d079700590de66687fa0b34e766e540de7d7ddb
cvetkovskin <cvetkovskinjp@gmail.com>
2018-05-29 21:58:51 +0900

    Combining the two forms into one

--- src/router/views/login/index.vue
+++ src/router/views/login/index.vue
@@ -13,52 +13,88 @@ export default {
     return {
       username: '',
       password: '',
-      signUpUsername: '',
-      signUpPassword: '',
       authError: null,
-      tryingToLogIn: false,
-      tryingToSignUp: false,
-      logInForm: true,
+      inProgress: false,
+      formProperties: {},
     }
+  },
+  created() {
+    this.showLogInForm()
   },
   methods: {
     // Try to log the user in with the username
     // and password they provided.
     logIn() {
-      // Reset the authError if it existed.
-      this.authError = null
-      this.tryingToLogIn = true
+      this.clearErrors()
+      this.inProgress = true
 
       // This redirects to home page
       return authorizeSelf(this.username, this.password).catch(error => {
-        this.tryingToLogIn = false
-        this.authError = error
+        this.inProgress = false
+        this.setError(error)
       })
     },
     // Try to create a new account for the user
     // with the username and password they provided.
     signUp() {
-      // Reset the authError if it existed.
-      this.authError = null
-      this.tryingToSignUp = true
+      this.clearErrors()
+      this.inProgress = true
 
       // This redirects to home page
-      return signupSelf(this.signUpUsername, this.signUpPassword)
+      return signupSelf(this.username, this.password)
         .catch(error => {
-          this.tryingToSignUp = false
-          this.authError = error
+          this.inProgress = false
+          this.setError(error)
         })
         .then(() => {
-          this.tryingToSignUp = false
+          this.inProgress = false
         })
     },
 
     showSignUpForm() {
-      this.logInForm = false
+      this.formProperties = {
+        title: 'Create a new account',
+        buttonText: 'Sign up',
+        leftLinkText: '',
+        leftAction: () => {},
+        rightLinkText: 'Already have an account',
+        rightAction: this.showLogInForm,
+        action: this.signUp,
+      }
+      this.clearErrors()
+      this.clearContent()
     },
 
     showLogInForm() {
-      this.logInForm = true
+      this.formProperties = {
+        title: 'Login to access your account',
+        buttonText: 'Log in',
+        leftLinkText: 'Forgot your password?',
+        leftAction: () => {},
+        rightLinkText: 'Create an account',
+        rightAction: this.showSignUpForm,
+        action: this.logIn,
+      }
+      this.clearErrors()
+      this.clearContent()
+    },
+
+    clearErrors() {
+      // Reset the authError if it existed.
+      this.authError = null
+    },
+
+    clearContent() {
+      this.username = ''
+      this.password = ''
+    },
+
+    setError(error) {
+      if (typeof error.description === 'string') {
+        this.authError = error.description
+      } else {
+        this.authError = error.policy
+      }
     },
   },
 }
@@ -67,13 +103,18 @@ export default {
 <template>
   <Layout>
     <div
-      v-if="logInForm"
       :class="$style.loginFormContainer"
     >
-      <p :class="$style.loginTitle">Login to access your account</p>
+      <p :class="$style.loginTitle">{{ formProperties.title }}</p>
+      <div
+        v-if="authError"
+        :class="$style.errorContainer"
+      >
+        <div :class="$style.textKeepNewLine">{{ authError }}</div>
+      </div>
       <form
         :class="$style.form"
-        @submit.prevent="logIn"
+        @submit.prevent="formProperties.action"
       >
         <BaseInput
           v-model="username"
@@ -87,78 +128,38 @@ export default {
           placeholder="Password"
         />
         <BaseButton
-          :disabled="tryingToLogIn || tryingToSignUp"
+          :disabled="inProgress"
           :class="$style.largeButton"
           type="submit"
         >
           <BaseIcon
-            v-if="tryingToLogIn || tryingToSignUp"
+            v-if="inProgress"
             name="sync"
             spin
           />
-          <span v-else>Log in</span>
+          <span v-else>{{ formProperties.buttonText }}</span>
         </BaseButton>
-        <p v-if="authError">
-          There was an error logging in to your account.
-        </p>
         <div :class="$style.loginFormFooter">
-          <span :class="$style.pullLeft">
-            <a :class="$style.clickable">
-              Forgot your password?
-            </a>
-          </span>
-          <span :class="$style.pullRight">
+          <span
+            :v-if="formProperties.leftLinkText"
+            :class="$style.pullLeft"
+          >
             <a
               :class="$style.clickable"
-              @click="showSignUpForm"
+              @click="formProperties.leftAction"
             >
-              Create an account
+              {{ formProperties.leftLinkText }}
             </a>
           </span>
-        </div>
-      </form>
-    </div>
-    <div
-      v-if="!logInForm"
-      :class="$style.loginFormContainer"
-    >
-      <p :class="$style.loginTitle">Create a new account</p>
-      <form
-        :class="$style.form"
-        @submit.prevent="signUp"
-      >
-        <BaseInput
-          v-model="signUpUsername"
-          name="signUpUsername"
-          placeholder="Email"
-        />
-        <BaseInput
-          v-model="signUpPassword"
-          name="signUpPassword"
-          type="password"
-          placeholder="Password"
-        />
-        <BaseButton
-          :disabled="tryingToLogIn || tryingToSignUp"
-          :class="$style.largeButton"
-          type="submit"
-        >
-          <BaseIcon
-            v-if="tryingToLogIn || tryingToSignUp"
-            name="sync"
-            spin
-          />
-          <span v-else>Sign up</span>
-        </BaseButton>
-        <p v-if="authError">
-          There was an error logging in to your account.
-        </p>
-        <div :class="$style.loginFormFooter">
-          <span :class="$style.pullRight">
+          <span
+            :v-if="formProperties.rightLinkText"
+            :class="$style.pullRight"
+          >
             <a
               :class="$style.clickable"
-              @click="showLogInForm">
-              Already have an account
+              @click="formProperties.rightAction"
+            >
+              {{ formProperties.rightLinkText }}
             </a>
           </span>
         </div>
@@ -204,6 +205,18 @@ export default {
 
 .pullRight {
   float: right;
+}
+
+.errorContainer {
+  padding: 10px;
+  margin-bottom: 20px;
+  color: white;
+  text-align: left;
+  background: #f84b4b;
+}
+
+.textKeepNewLine {
+  white-space: pre-line;
 }
 
 .form {
