96cd163fb57e5c9c37bc1bccc1bc57cea21f2ff2
Sbalbp <sbalbp@gmail.com>
2018-07-22 00:34:57 +0900

    Add PostList component

---
+++ src/components/post-list.vue
@@ -0,0 +1,47 @@
+<script>
+import PostContainer from '@components/post-container'
+
+export default {
+  components: { PostContainer },
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+    showAuthor: {
+      type: Boolean,
+      default: false,
+    },
+    editable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<template>
+  <div>
+    <div
+      v-for="(post, index) in posts"
+      :key="post.title">
+      <PostContainer
+        :post="post"
+        :show-author="showAuthor"
+        :editable="editable"
+        :class="$style.postContainer"
+        @save-draft="$emit('save-draft', index, $event)"
+        @publish-draft="$emit('publish-draft', index, $event)"
+      />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" module>
+@import '@design';
+
+.postContainer {
+  width: 90%;
+  margin: 20px auto;
+}
+</style>
