7b9ef5ffced0b45ad75c96417827928454840e38
Sbalbp <sbalbp@gmail.com>
2018-08-04 20:20:35 +0900

    Add updatePost mutation

--- server/src/resolvers/mutation/post.js
+++ server/src/resolvers/mutation/post.js
@@ -35,6 +35,25 @@ export default {
     )
   },
 
+  async updatePost(parent, { id, title, text }, ctx, info) {
+    const userId = ctx.request.user.id
+    const postExists = await ctx.db.exists.Post({
+      id,
+      author: { id: userId },
+    })
+    if (!postExists) {
+      throw new Error(`Post not found or you're not the author`)
+    }
+
+    return ctx.db.mutation.updatePost(
+      {
+        where: { id },
+        data: { title, text },
+      },
+      info
+    )
+  },
+
   async deletePost(parent, { id }, ctx, info) {
     const userId = ctx.req.user.id
     const postExists = await ctx.db.exists.Post({
