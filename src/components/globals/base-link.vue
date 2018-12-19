6e2b262eb6aa916c08d845255ab70d1418ee2f8c
Chris Fritz <chrisvfritz@gmail.com>
2018-12-19 12:38:55 -0500

    allow insecure mailto and tel protocols in base-link

--- src/components/globals/base-link.vue
+++ src/components/globals/base-link.vue
@@ -51,7 +51,7 @@ export default {
           )
         }
         // Check for insecure URL in href.
-        if (!this.allowInsecure && !this.href.startsWith('https')) {
+        if (!this.allowInsecure && !/^(https|mailto|tel):/.test(this.href)) {
           return console.warn(
             `Insecure <BaseLink> href: ${
               this.href
