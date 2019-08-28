4f160053c085cd869a86049607774c941f818fae
Fran Dios <frankdiox@gmail.com>
2019-08-29 02:17:51 +0900

    Split test into two projects with different environments

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
-const _ = require('lodash')
-
 // @ts-ignore
 const { paths } = require('./jsconfig.json').compilerOptions
 const aliasesMapper = Object.keys(paths).reduce(
@@ -11,26 +9,52 @@ const aliasesMapper = Object.keys(paths).reduce(
   {}
 )
 
-module.exports = {
-  setupFiles: ['<rootDir>/tests/unit/setup'],
-  globalSetup: '<rootDir>/tests/unit/global-setup',
-  globalTeardown: '<rootDir>/tests/unit/global-teardown',
-  setupTestFrameworkScriptFile: '<rootDir>/tests/unit/matchers',
-  testMatch: ['**/(*.)unit.js'],
-  moduleFileExtensions: ['js', 'json', 'vue', 'gql', 'graphql'],
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '^.+\\.js$': 'babel-jest',
-    '\\.(gql|graphql)$': 'jest-transform-graphql',
-  },
+const common = {
   transformIgnorePatterns: ['/node_modules/(?!vue-cli-plugin-apollo).+\\.js$'],
+  moduleFileExtensions: ['js', 'json', 'vue', 'gql', 'graphql'],
   moduleNameMapper: {
     // Transform any static assets to empty strings
     '\\.(jpe?g|png|gif|webp|svg|mp4|webm|ogg|mp3|wav|flac|aac|woff2?|eot|ttf|otf)$':
       '<rootDir>/tests/unit/fixtures/empty-string.js',
     ...aliasesMapper,
   },
-  snapshotSerializers: ['jest-serializer-vue'],
+}
+
+module.exports = {
+  projects: [
+    {
+      ...common,
+      displayName: 'frontend',
+      testEnvironment: 'jsdom',
+      setupFiles: ['<rootDir>/tests/unit/setup'],
+      setupTestFrameworkScriptFile: '<rootDir>/tests/unit/matchers',
+      testMatch: ['<rootDir>/src/**/(*.)unit.js'],
+      snapshotSerializers: ['jest-serializer-vue'],
+      transform: {
+        '^.+\\.vue$': 'vue-jest',
+        '^.+\\.js$': 'babel-jest',
+        '\\.(gql|graphql)$': 'jest-transform-graphql',
+      },
+      globals: {
+        'vue-jest': {
+          // Disable CSS compilation until it's more stable (doesn't work with sass-dart)
+          experimentalCSSCompile: false,
+        },
+      },
+    },
+    {
+      ...common,
+      displayName: 'backend',
+      testEnvironment: 'node',
+      testMatch: ['<rootDir>/server/src/**/(*.)unit.js'],
+      setupFiles: [],
+      transformIgnorePatterns: [
+        '/node_modules/(?!apollo-datasource|apollo-server-errors)',
+      ],
+    },
+  ],
+  globalSetup: '<rootDir>/tests/unit/global-setup',
+  globalTeardown: '<rootDir>/tests/unit/global-teardown',
   coverageDirectory: '<rootDir>/tests/unit/coverage',
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
@@ -46,15 +70,4 @@ module.exports = {
     '!server/src/resolvers/**',
     '!server/src/**/_*.js',
   ],
-  // https://facebook.github.io/jest/docs/en/configuration.html#testurl-string
-  // Set the `testURL` to a provided base URL if one exists, or the mock API base URL
-  // Solves: https://stackoverflow.com/questions/42677387/jest-returns-network-error-when-doing-an-authenticated-request-with-axios
-  testURL:
-    process.env.API_BASE_URL || `http://localhost:${_.random(9000, 9999)}`,
-  globals: {
-    'vue-jest': {
-      // Disable CSS compilation until it's more stable (doesn't work with sass-dart)
-      experimentalCSSCompile: false,
-    },
-  },
 }
